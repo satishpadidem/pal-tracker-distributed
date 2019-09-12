64bd3eb10de777f4c551bc305acf563cf277bfe3
PAL-5 <manjari_s@dell.com>
2019-09-12 16:34:00 +0530

    added OAUTH

--- Components/Backlog/ProjectClient.cs
+++ Components/Backlog/ProjectClient.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -8,13 +10,15 @@ namespace Backlog
 {
     public class ProjectClient : IProjectClient
     {
-        private readonly HttpClient _client;
+       private readonly HttpClient _client;
         private readonly ILogger<ProjectClient> _logger;
         private readonly IDictionary<long, ProjectInfo> _projectCache = new Dictionary<long, ProjectInfo>();
-        public ProjectClient(HttpClient client,ILogger<ProjectClient> logger)
+        private readonly Func<Task<string>> _accessTokenFn;
+        public ProjectClient(HttpClient client,ILogger<ProjectClient> logger, Func<Task<string>> accessTokenFn)
         {
             _client = client;
             _logger = logger;
+            _accessTokenFn = accessTokenFn;
         }
 
         public async Task<ProjectInfo> Get(long projectId)  => await new GetProjectCommand(DoGet,DoGetFromCache,projectId).ExecuteAsync();
@@ -22,6 +26,12 @@ namespace Backlog
 
          public async Task<ProjectInfo> DoGet(long projectId)
          {
+
+              var token = await _accessTokenFn();
+
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            
              _client.DefaultRequestHeaders.Accept.Clear();
             var streamTask = _client.GetStreamAsync($"project?projectId={projectId}");
             _logger.LogInformation($"Attempting to fetch projectId: {projectId}");
